1328ff9179998d8e21fd0f88020d085bc5add5a8
erick.gomes <erick.gomes@animaeducacao.com.br>
2022-01-13 22:17:05 -0300

    foi melhorando o css, colado imagem de fundo e melhorado o php galeria de cabecalho

--- cadastrar.php
+++ cadastrar.php
@@ -1,27 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php include "cabecalho.php" ?>
 
-    
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-    <title>Video Cine</title>
-</head>
 <body>
 
 <nav class="nav-extended grey darken-4">
     <div class="nav-wrapper">
         <ul id="nav-mobile" class="right">
           <li><a href="galeria.php">Galeria</a></li>
-          <li><a href="cadastrar.php">Cadastrar</a></li>
+          <li class="active"><a href="cadastrar.php">Cadastrar</a></li>
       </ul>
     </div>
     <div class="nav-header center">

b12d2b4452b39f74e02c5b7dab3473df7ff560cb
erick.gomes <erick.gomes@animaeducacao.com.br>
2022-01-16 16:47:05 -0300

    foi melhorado o código com array e foreach com a lista de filmes e serie

--- galeria.php
+++ galeria.php
@@ -1,5 +1,36 @@
 <?php include "cabecalho.php" ?>
 
+<?php
+
+$filme1 = [
+    "titulo"=>"Jobs: O filme",
+    "nota" => 10,
+    "sinopse" => "A história da ascensão de Steve Jobs, de rejeitado no colégio até tornar-se um dos mais reverenciados empresários do universo da tecnologia no século 20. A trama passa pela jornada de auto descobrimento da juventude, pelos demônios pessoais que obscureceram sua visão e, finalmente, pelos triunfos que transformaram sua vida adulta.",
+    "poster" => "https://www.themoviedb.org/t/p/original/5LUplLrA0jeIOKhMie4XU54uAIO.jpg"
+];
+$filme2 = [
+    "titulo"=>"Homem-Aranha: Sem Volta para Casa",
+    "nota" => 10,
+    "sinopse" => "Peter Parker é desmascarado e não consegue mais separar sua vida normal dos grandes riscos de ser um super-herói. Quando ele pede ajuda ao Doutor Estranho, os riscos se tornam ainda mais perigosos, e o forçam a descobrir o que realmente significa ser o Homem-Aranha.",
+    "poster" => "https://www.themoviedb.org/t/p/w300/fVzXp3NwovUlLe7fvoRynCmBPNc.jpg"
+];
+$filme3 = [
+    "titulo"=>"Star Wars: O livro de Boba Fett",
+    "nota" => 10,
+    "sinopse" => "Uma emocionante aventura Star Wars que encontra o lendário caçador de recompensas Boba Fett e o mercenário Fennec Shand explorando o submundo da Galáxia quando retornam às areias de Tatooine para reivindicar o território antes governado por Jabba, o Hutt e seu sindicato do crime.",
+    "poster" => "https://www.themoviedb.org/t/p/w300/kVelEAAPWMMXCNwsX7YVE8iOlYs.jpg"
+];
+$filme4 = [
+    "titulo"=>"Star Wars: O Mandaloriano",
+    "nota" => 10,
+    "sinopse" => "A saga de um guerreiro solitário, que também é um mercenário e pistoleiro, viajando pelos territórios esquecidos e marginais do espaço, logo após a queda do Império e antes da criação da temida Primeira Ordem.",
+    "poster" => "https://www.themoviedb.org/t/p/w300/A8SP1xPKnq7XunB5Pwwpjm5SrvN.jpg"
+];
+
+$filmes = [$filme1 , $filme2, $filme3, $filme4];
+
+?>
+
 <body>
 
 <nav class="nav-extended grey darken-4">
@@ -22,85 +53,30 @@
   </nav>
 
   <div class="row">
+    <?php 
+        foreach($filmes as $filme) : ?>
         <div class="col s3">
-        <div class="card hoverable">
-            <div class="card-image">
-            <img src="https://www.themoviedb.org/t/p/w300/5LUplLrA0jeIOKhMie4XU54uAIO.jpg">
-            <span class="card-title">Jobs</span>
-            <a class="btn-floating halfway-fab waves-effect waves-light red">
-                <i class="material-icons">favorite_border</i>
-            </a>
-            </div>
-            <div class="card-content">
-                <p class="valign-wrapper">
-                    <i class="material-icons amber-text">star</i> 10
-                </p>
-                <p>A história da ascensão de Steve Jobs, de rejeitado no colégio até tornar-se um dos mais reverenciados empresários do universo da tecnologia no século 20. A trama passa pela jornada de auto descobrimento da juventude, pelos demônios pessoais que obscureceram sua visão e, finalmente, pelos triunfos que transformaram sua vida adulta.</p>
-            </div>
-        </div>
-
-      </div>
-
-      <div class="col s3">
-        <div class="card hoverable">
-            <div class="card-image">
-            <img src="https://www.themoviedb.org/t/p/w300/fVzXp3NwovUlLe7fvoRynCmBPNc.jpg">
-            <a class="btn-floating halfway-fab waves-effect waves-light red">
-                <i class="material-icons">favorite_border</i>
-            </a>
-            </div>
-            <div class="card-content">
-                <p class="valign-wrapper">
-                    <i class="material-icons amber-text">star</i> 10
-                </p>
-                <span class="card-title">Homem-Aranha</span>
-
-                <p>Peter Parker é desmascarado e não consegue mais separar sua vida normal dos grandes riscos de ser um super-herói. Quando ele pede ajuda ao Doutor Estranho, os riscos se tornam ainda mais perigosos, e o forçam a descobrir o que realmente significa ser o Homem-Aranha.</p>
+            <div class="card hoverable">
+                <div class="card-image">
+                <img src="<?= $filme["poster"] ?>">
+                <a class="btn-floating halfway-fab waves-effect waves-light red">
+                    <i class="material-icons">favorite_border</i>
+                </a>
+                </div>
+                <div class="card-content">
+                    <p class="valign-wrapper">
+                        <i class="material-icons amber-text">star</i> <?= $filme["nota"] ?>
+                    </p>
+                    <span class="card-title"><?= $filme["titulo"] ?></span>
+                    <p><?= $filme["sinopse"] ?></p>
+                </div>
             </div>
         </div>
+      <?php endforeach ?>
 
-      </div>
-
-      <div class="col s3">
-        <div class="card hoverable">
-            <div class="card-image">
-            <img src="https://www.themoviedb.org/t/p/w300/kVelEAAPWMMXCNwsX7YVE8iOlYs.jpg">
-            <a class="btn-floating halfway-fab waves-effect waves-light red">
-                <i class="material-icons">favorite_border</i>
-            </a>
-            </div>
-            <div class="card-content">
-                <p class="valign-wrapper">
-                    <i class="material-icons amber-text">star</i> 10
-                </p>
-                <span class="card-title">O Livro de Boba Fett</span>
-
-                <p>Uma emocionante aventura Star Wars que encontra o lendário caçador de recompensas Boba Fett e o mercenário Fennec Shand explorando o submundo da Galáxia quando retornam às areias de Tatooine para reivindicar o território antes governado por Jabba, o Hutt e seu sindicato do crime.</p>
-           </div>
-        </div>
-
-      </div>
-
-        <div class="col s3">
-        <div class="card hoverable">
-            <div class="card-image">
-            <img src="https://www.themoviedb.org/t/p/w300/A8SP1xPKnq7XunB5Pwwpjm5SrvN.jpg">
-            <a class="btn-floating halfway-fab waves-effect waves-light red">
-                <i class="material-icons">favorite_border</i>
-            </a>
-            </div>
-            <div class="card-content">
-                <p class="valign-wrapper">
-                    <i class="material-icons amber-text">star</i> 10
-                </p>
-                <span class="card-title">The Mandalorian</span>
-
-                <p>A saga de um guerreiro solitário, que também é um mercenário e pistoleiro, viajando pelos territórios esquecidos e marginais do espaço, logo após a queda do Império e antes da criação da temida Primeira Ordem.</p>
-            </div>
-        </div>
-
-      </div>
   </div>
+</div>
+</div>
 
   
 </body>
